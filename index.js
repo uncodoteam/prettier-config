2353890f924f8675743dd6db85dd9aaf1bdcc8d5
nickxsy <104327959+nickozy@users.noreply.github.com>
2025-05-01 22:58:35 +0300

    init

---
+++ index.js
@@ -0,0 +1,24 @@
+module.exports = {
+  bracketSpacing: false,
+  tabWidth: 2,
+  printWidth: 80,
+  singleQuote: true,
+  trailingComma: 'all',
+  useTabs: false,
+  semi: true,
+  arrowParens: 'avoid',
+  overrides: [
+    {
+      files: ['*.js', '*.jsx', '*.ts', '*.tsx'],
+      options: {
+        parser: 'typescript',
+      },
+    },
+    {
+      files: ['*.md', '*.json', '*.yaml', '*.yml'],
+      options: {
+        tabWidth: 2,
+      },
+    },
+  ],
+};
